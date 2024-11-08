9d179e1d4c1987d9cf573025a56fe4b001b8017d
khrisharrison <khristianhfs06@gmail.com>
2024-11-07 23:42:28 -0400

    Se crea API Rest para enviar precio

--- apps/back-venta-boletos/src/app/app.module.ts
+++ apps/back-venta-boletos/src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrecioModule } from './precio/precio.module';
 
 @Module({
-  imports: [],
+  imports: [PrecioModule],
   controllers: [AppController],
   providers: [AppService],
 })
