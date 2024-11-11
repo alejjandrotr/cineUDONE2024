3be576b3bf438f389119674d7e7e2e956d417d2a
JesusMarichal <jesusmarichal0@gmail.com>
2024-11-11 12:09:53 -0400

    agregando los modulos a app.module.ts

--- apps/back-venta-boletos/src/app/app.module.ts
+++ apps/back-venta-boletos/src/app/app.module.ts
@@ -5,7 +5,9 @@ import { TypeOrmModule } from '@nestjs/typeorm'
 import { Paymentinfo } from './paymentinfo/paymentinfo.entity';
 import { PaymentinfoModule } from './paymentinfo/paymentinfo.module';
 import {createpagomovil} from './pago-movil/pago-movil.entity';
+import {PagoTransferencia} from './pago-transferencia/pago-transferencia.entity'
 import {pago_movil_module} from './pago-movil/pago-movil.module';
+import { PagoTransferenciaModule } from './pago-transferencia/pago-transferencia.module';
 
 @Module({
   imports: [
@@ -16,11 +18,12 @@ import {pago_movil_module} from './pago-movil/pago-movil.module';
       username: 'root',
       password: '',
       database: 'cine',
-      entities: [Paymentinfo, createpagomovil],
+      entities: [Paymentinfo, createpagomovil, PagoTransferencia],
       synchronize: true
     }),
     PaymentinfoModule,
     pago_movil_module,
+    PagoTransferenciaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
