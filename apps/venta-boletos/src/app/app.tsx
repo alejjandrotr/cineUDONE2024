9e111f793bf644e14dc0788b1e95a7550bb1da53
GabozAlex <gaboalex9@gmail.com>
2025-01-29 09:23:12 -0400

    Anadir interfaz Inicio Sesion

--- apps/venta-boletos/src/app/app.tsx
+++ apps/venta-boletos/src/app/app.tsx
@@ -1,50 +1,24 @@
-import NxWelcome from './nx-welcome';
+import React, { useState } from 'react';
+import LoginForm from '../components/LoginForm';
+import RegisterForm from '../components/RegisterForm';
+import '../styles.css';
 
-import { Route, Routes, Link } from 'react-router-dom';
+const App: React.FC = () => {
+  const [showLogin, setShowLogin] = useState(true);
 
-export function App() {
+  const toggleForm = () => {
+    setShowLogin(!showLogin);
+  };
+
   return (
-    <div>
-      <NxWelcome title="venta-boletos" />
-
-      {/* START: routes */}
-      {/* These routes and navigation have been generated for you */}
-      {/* Feel free to move and update them to fit your needs */}
-      <br />
-      <hr />
-      <br />
-      <div role="navigation">
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/page-2">Page 2</Link>
-          </li>
-        </ul>
-      </div>
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div>
-              This is the generated root route.{' '}
-              <Link to="/page-2">Click here for page 2.</Link>
-            </div>
-          }
-        />
-        <Route
-          path="/page-2"
-          element={
-            <div>
-              <Link to="/">Click here to go back to root page.</Link>
-            </div>
-          }
-        />
-      </Routes>
-      {/* END: routes */}
+    <div className="app">
+      {showLogin ? (
+        <LoginForm onToggle={toggleForm} />
+      ) : (
+        <RegisterForm onToggle={toggleForm} />
+      )}
     </div>
   );
-}
+};
 
 export default App;
