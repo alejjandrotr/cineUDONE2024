671fb876191e767e2385babebcc253491284db40
Alex <alexurbina616@gmail.com>
2025-02-04 22:44:29 -0400

    Api añadir peliculas

--- apps/back-cartelera-cine/src/app/app.controller.ts
+++ apps/back-cartelera-cine/src/app/app.controller.ts
@@ -1,13 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-
+import { Controller, Post, Body, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
+import { MoviesService } from '../movies/service/movies.service';
+import { CreateMovieDto } from '../movies/dto/create-movie.dto';
+//import { AuthGuard } from '../auth/guards/auth.guard'; 
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly moviesService: MoviesService
+  ) {}
 
   @Get()
   getData() {
     return this.appService.getData();
+  }
+
+  //@UseGuards(AuthGuard) 
+  @Post('movies')
+  addMovie(@Body() movieData: CreateMovieDto) {
+    return this.moviesService.addMovie(movieData);
+  }
+
+  @Get('movies')
+  getMovies() {
+    return this.moviesService.getAllMovies();
   }
 }
---
+++ apps/back-cartelera-cine/src/movies/entity/movie.entity.ts
@@ -0,0 +1,13 @@
+export class Movie {
+  id: number;
+  title: string;
+  synopsis: string;
+  genre: number[];
+  schedule: { date: string; room: string }[];
+  type: string;
+  rating: string;
+  duration: number;
+  poster: string;
+  price: number;
+  trailerUrl: string;
+}
