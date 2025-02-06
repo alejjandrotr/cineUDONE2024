0aa68b7b44fe086258487abf92fe838bc227e795
Alex <alexurbina616@gmail.com>
2025-02-06 18:37:12 -0400

    Prueba test front

--- apps/cartelera-cine/src/app/app.spec.tsx
+++ apps/cartelera-cine/src/app/app.spec.tsx
@@ -1,25 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './app';
+import App from './App'; 
 
 describe('App', () => {
-  it('should render successfully', () => {
-    const { baseElement } = render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    );
-    expect(baseElement).toBeTruthy(); 
-  });
-
-  it('should display "Page 2" when navigating to the /page-2 route', () => {
+  it('should render the Header component', () => {
     render(
       <BrowserRouter>
         <App />
       </BrowserRouter>
     );
 
-    const pageText = screen.getByText(/Page 2/i);
-    expect(pageText).not.toBeNull(); 
+    const headerTitle = screen.getByText('CINE'); 
+    expect(headerTitle).toBeTruthy(); 
   });
 });
