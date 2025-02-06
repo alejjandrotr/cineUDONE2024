1cbbee218562255bdd89ab41688f17510542100c
AlexU616 <166437026+AlexU616@users.noreply.github.com>
2025-02-06 18:47:06 -0400

    Update app.spec.tsx

--- apps/cartelera-cine/src/app/app.spec.tsx
+++ apps/cartelera-cine/src/app/app.spec.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
-import App from './App'; 
+import App from './app'; 
 
 describe('App', () => {
   it('should render the Header component', () => {
