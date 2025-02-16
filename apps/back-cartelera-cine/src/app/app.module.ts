84649275803f01fe9c75313563782245220aba71
Alexandra Quijada <alexandraquijada84@gmail.com>
2025-02-16 01:04:22 -0400

    Añadidos archivos rastreados

--- apps/back-cartelera-cine/src/app/app.module.ts
+++ apps/back-cartelera-cine/src/app/app.module.ts
@@ -1,10 +1,24 @@
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AggpeliModule } from './aggpeli/aggpeli.module';
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql', //para conectarse
+      host:'localhost',
+      port:3306,
+      username:'root',
+      password:'1234',
+      database: 'cartelera',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
+    AggpeliModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- apps/cartelera-cine/src/components/SideBar.tsx
+++ apps/cartelera-cine/src/components/SideBar.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import '../styles/SideBar.css';
 import logo from '../assets/logo.png';
-import { FaHome, FaFilm, FaUtensils, FaStar, FaInstagram, FaFacebook } from 'react-icons/fa';
+import { FaHome, FaFilm, FaUtensils, FaStar, FaInstagram, FaFacebook, FaUserCog } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate
 
 const Sidebar = () => {
+  const navigate = useNavigate(); // Inicializa useNavigate
+
+  // Función para redirigir a la interfaz de admin
+  const goToAdmin = () => {
+    navigate('/admin');
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar-header">
@@ -26,6 +34,11 @@ const Sidebar = () => {
           <li>
             <FaStar />
             <span>&nbsp;Próximos Estrenos</span>
+          </li>
+          {/* Botón para ir a la interfaz de admin */}
+          <li onClick={goToAdmin} style={{ cursor: 'pointer' }}>
+            <FaUserCog />
+            <span>&nbsp;Admin</span>
           </li>
           {/* Redes sociales */}
           <li>
